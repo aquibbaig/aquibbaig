a30c3c045fe23cb7974f4683d0904503676bc701
Aquib Baig <aquibbaig97@gmail.com>
2020-06-06 20:27:05 +0530

    add scroll animation

--- js/main.js
+++ js/main.js
@@ -7,6 +7,30 @@ $(document).ready(function() {
     $nav.toggleClass('collapse');
   });
 
+  // Add smooth scroll to links
+  // Add smooth scrolling to all links
+  $(".scroll").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
   // Show only 3 projects on
   // the start.
   parseFilterProjectOptions();
